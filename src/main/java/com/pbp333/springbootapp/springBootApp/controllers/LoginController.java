d8a03e64b35202edcf22c05fdcb2d7ab8d5ad6e8
Pedro Pereira <pbp333@gmail.com>
2018-01-09 22:15:06 +0000

    welcome page test

---
+++ src/main/java/com/pbp333/springbootapp/springBootApp/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package com.pbp333.springbootapp.springBootApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String loginMessage(){
+        return "welcome modi";
+    }
+
+
+}
